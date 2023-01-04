365fe829d9df448835654a31bf3d3d0405b1ee25
diana-sy <70268539+diana-sy@users.noreply.github.com>
2023-01-04 22:16:09 +0200

    Changed: added variable so camera works

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const video = document.querySelector("video")
 
 async function setup(){
-   await navigator.mediaDevices.getUserMedia({video:true})
+  const stream = await navigator.mediaDevices.getUserMedia({video:true})
    video.srcObject = stream 
 }
 
